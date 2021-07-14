1b9b80d6893cc1bcf7b270343bcb0f51c7a9a4ae
NeoNeonilla <nelliafleurova@gmail.com>
2021-07-14 15:38:04 -0500

    shuffle function

--- script.js
+++ script.js
@@ -23,6 +23,17 @@ var numericCharacters =  ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = [  '@',  '%',  '+',  '\\',  '/',  "'",  '!',  '#',  '$',  '^',  '?',  ':',  ',',  ')',  '(',  '}',  '{',  ']',  '[',  '~',  '-',  '_',  '.'];
 var lowerCase = [  'a',  'b',  'c',  'd',  'e',  "f",  'g',  'h',  'i',  'j',  'k',  'l',  'm',  'n',  'o',  'p',  'q',  'r',  's',  't',  'u',  'v',  'w',  'x',  'y',  'z'];
 var upperCase = [  'A',  'B',  'C',  'D',  'E',  "F",  'G',  'H',  'I',  'J',  'K',  'L',  'M',  'N', 'O',  'P',  'Q',  'R',  'S',  'R',  'U',  'V',  'W',  'X',  'Y',  'Z'];
+                   
+function shuffle (array){
+  //grabs a randome index from selected array
+var chooseRandomIndex = (Math.floor(Math.random * array.length)) 
+//gives actual value of random index seleected above
+var indexValue = array[chooseRandomIndex] 
+return indexValue;
+}
+
+
+
 
 function userPrompt (){
   var passwordLimit = prompt ("How long would you like your password to be?")
@@ -36,6 +47,22 @@ var confirmNumbers = confirm("Would you like to have numbers in your password?")
 var confirmCharacters = confirm("Would you like to use special Characters?")
 var confirmLower = confirm("would you like to use lower case letters?")
 var confirmUpper = confirm("would you like to use Upper case letters?")
+
+
+var userChoice = {
+numeric: confirmNumbers,
+special: confirmCharacters,
+lower: confirmLower,
+upper: confirmUpper,
+
+length: passwordLimit
+}
+console.log(userChoice)
+return userChoice
+
+
+
+
 }
 
 
